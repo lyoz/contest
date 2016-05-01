5988865c72c2e5ca78b0af7bd8880dee54997328
lyoz <lyoz@users.noreply.github.com>
2016-05-02 01:11:04 +0900

    codejam 2016 round1a

---
+++ codejam/2016/round1a/B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) cout<<"# "<<#__VA_ARGS__<<'='<<(__VA_ARGS__)<<endl
+
+void solve()
+{
+}
+
+int main()
+{
+	int T; cin>>T;
+	for(int _case=1;_case<=T;_case++){
+		cout<<"Case #"<<_case<<": ";
+		solve();
+	}
+}
