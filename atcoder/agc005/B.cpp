a85bfc1c6d2bde454e3ad37bf5348fcc07afdbc0
lyoz <lyoz@users.noreply.github.com>
2016-11-07 02:56:58 +0900

    AGC005 Problem ABC

--- atcoder/agc005/B.cpp
+++ atcoder/agc005/B.cpp
@@ -1,76 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define dump(...) cout<<"# "<<#__VA_ARGS__<<'='<<(__VA_ARGS__)<<endl
-#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
-#define peri(i,a,b) for(int i=int(b);i-->int(a);)
-#define rep(i,n) repi(i,0,n)
-#define per(i,n) peri(i,0,n)
-#define all(c) begin(c),end(c)
-#define mp make_pair
-#define mt make_tuple
-
-using uint=unsigned;
 using ll=long long;
-using ull=unsigned long long;
-using vi=vector<int>;
-using vvi=vector<vi>;
-using vl=vector<ll>;
-using vvl=vector<vl>;
-using vd=vector<double>;
-using vvd=vector<vd>;
-using vs=vector<string>;
-
-template<typename T1,typename T2>
-ostream& operator<<(ostream& os,const pair<T1,T2>& p){
-	return os<<'('<<p.first<<','<<p.second<<')';
-}
-
-template<typename Tuple>
-void print_tuple(ostream&,const Tuple&){}
-template<typename Car,typename... Cdr,typename Tuple>
-void print_tuple(ostream& os,const Tuple& t){
-	print_tuple<Cdr...>(os,t);
-	os<<(sizeof...(Cdr)?",":"")<<get<sizeof...(Cdr)>(t);
-}
-template<typename... Args>
-ostream& operator<<(ostream& os,const tuple<Args...>& t){
-	print_tuple<Args...>(os<<'(',t);
-	return os<<')';
-}
-
-template<typename Ch,typename Tr,typename C>
-basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
-	os<<'[';
-	for(auto i=begin(c);i!=end(c);++i)
-		os<<(i==begin(c)?"":" ")<<*i;
-	return os<<']';
-}
-
-constexpr int INF=1e9;
-constexpr int MOD=1e9+7;
-constexpr double EPS=1e-9;
 
 int main()
 {
 	for(int n;cin>>n && n;){
-		vi a(n),b(n+1,-1);
-		rep(i,n){
-			cin>>a[i];
-			b[a[i]]=i;
-		}
-		map<int,int> f;
-		f[0]=n;
+		vector<int> a(n),b(n);
+		for(int i=0;i<n;i++)
+			cin>>a[i],b[--a[i]]=i;
+		map<int,int> range;
+		range[0]=n;
 		ll res=0;
-		repi(x,1,n+1){
-			int c=b[x];
-			auto i=--f.upper_bound(c);
-			int l=i->first,r=i->second;
-			assert(l<=c && c<r);
-			res+=ll(x)*(c-l+1)*(r-c);
-			f.erase(i);
-			if(l<c) f[l]=c;
-			if(c+1<r) f[c+1]=r;
+		for(int x=0;x<n;x++){
+			int i=b[x];
+			auto it=--range.upper_bound(i);
+			int l=it->first,r=it->second;
+			res+=ll(x+1)*(i-l+1)*(r-i);
+			range.erase(it);
+			if(l<i) range[l]=i;
+			if(i+1<r) range[i+1]=r;
 		}
 		cout<<res<<endl;
 	}
--- atcoder/agc005/C.cpp
+++ atcoder/agc005/C.cpp
@@ -1,86 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define dump(...) cout<<"# "<<#__VA_ARGS__<<'='<<(__VA_ARGS__)<<endl
-#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
-#define peri(i,a,b) for(int i=int(b);i-->int(a);)
-#define rep(i,n) repi(i,0,n)
-#define per(i,n) peri(i,0,n)
-#define all(c) begin(c),end(c)
-#define mp make_pair
-#define mt make_tuple
-
-using uint=unsigned;
-using ll=long long;
-using ull=unsigned long long;
-using vi=vector<int>;
-using vvi=vector<vi>;
-using vl=vector<ll>;
-using vvl=vector<vl>;
-using vd=vector<double>;
-using vvd=vector<vd>;
-using vs=vector<string>;
-
-template<typename T1,typename T2>
-ostream& operator<<(ostream& os,const pair<T1,T2>& p){
-	return os<<'('<<p.first<<','<<p.second<<')';
-}
-
-template<typename Tuple>
-void print_tuple(ostream&,const Tuple&){}
-template<typename Car,typename... Cdr,typename Tuple>
-void print_tuple(ostream& os,const Tuple& t){
-	print_tuple<Cdr...>(os,t);
-	os<<(sizeof...(Cdr)?",":"")<<get<sizeof...(Cdr)>(t);
-}
-template<typename... Args>
-ostream& operator<<(ostream& os,const tuple<Args...>& t){
-	print_tuple<Args...>(os<<'(',t);
-	return os<<')';
-}
-
-template<typename Ch,typename Tr,typename C>
-basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
-	os<<'[';
-	for(auto i=begin(c);i!=end(c);++i)
-		os<<(i==begin(c)?"":" ")<<*i;
-	return os<<']';
-}
-
-constexpr int INF=1e9;
-constexpr int MOD=1e9+7;
-constexpr double EPS=1e-9;
-
-bool solve(vi a)
+bool solve(vector<int> a)
 {
-	int n=a.size();
-	sort(all(a));
-
-	if(a[0]!=(a[n-1]+1)/2)
+	int mn=*min_element(begin(a),end(a)),mx=*max_element(begin(a),end(a));
+	if(mn<(mx+1)/2)
 		return false;
-
-	int mn=a[0],mx=a[n-1];
-	if(a[n-1]&1){  // 直径が奇数
-		for(int x=mx;x!=mn;x--)
-			if(count(all(a),x)<2)
-				return false;
-		return count(all(a),a[0])==2;
-	}
-	else{  // 直径が偶数
-		for(int x=mx;x!=mn;x--)
-			if(count(all(a),x)<2)
-				return false;
-		return count(all(a),a[0])==1;
-	}
-
-	return true;
+	for(int x=mn+1;x<=mx;x++)
+		if(count(begin(a),end(a),x)<2)
+			return false;
+	if(mx&1)
+		return count(begin(a),end(a),mn)==2;
+	else
+		return count(begin(a),end(a),mn)==1;
 }
 
 int main()
 {
 	for(int n;cin>>n && n;){
-		vi a(n);
-		rep(i,n) cin>>a[i];
+		vector<int> a(n);
+		for(int& x:a) cin>>x;
 		cout<<(solve(a)?"Possible":"Impossible")<<endl;
 	}
 }
