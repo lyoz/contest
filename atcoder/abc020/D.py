3fd59ae3bcbcbb9d45a2bc8e42114ec6d966c453
lyoz <lyoz@users.noreply.github.com>
2017-01-05 03:59:07 +0900

    ABC020 Problem D

--- atcoder/abc020/D.py
+++ atcoder/abc020/D.py
@@ -1,49 +1,38 @@
 MOD=int(1e9+7)
 
-def gcd(a,b):
-    return gcd(b,a%b) if b else a
+def get_primes():
+    ps=[0,0]+[i for i in range(2,32000)]
+    for i in range(2,32000):
+        if ps[i]==0:
+            continue
+        for j in range(i*i,32000,i):
+            ps[j]=0
+    return [p for p in ps if p]
 
-def lcm(a,b):
-    return a//gcd(a,b)*b
-
-# a + a+d + ... + a+(n-1)d
-def get_sum(a,n,d):
-    return a*n+d*(n-1)*n//2
-
-# get primes \in [0,n)
-def get_primes(n):
-    res=[0]*n
-    for i in range(2,n):
-        res[i]=i
-    for i in range(2,n):
-        if res[i]:
-            for j in range(i*i,n,i):
-                res[j]=0
-    return [x for x in res if x!=0]
-
-# get divisors for n
-def get_divisors(n):
-    res=[]
-    i=1
-    while i*i<=n:
-        if n%i==0:
-            res.extend((i,n//i))
+def get_divisors(k):
+    i,ds=1,[]
+    while i*i<=k:
+        if k%i==0:
+            ds.append(i)
+            ds.append(k//i)
         i+=1
-    return sorted(set(res))
+    return sorted(set(ds))
 
-# get factors for n
-def get_factors(n,ps):
-    res=[]
+def get_factors(k,ps):
+    i,fs=1,[]
     for p in ps:
-        if p*p>n:
+        if p*p>k:
             break
-        if n%p==0:
-            res.append(p)
-        while n%p==0:
-            n//=p
-    if n>1:
-        res.append(n)
-    return res
+        if k%p==0:
+            fs.append(p)
+        while k%p==0:
+            k//=p
+    if k>1:
+        fs.append(k)
+    return fs
+
+def get_sum(a,d,n):
+    return (a+a+(n-1)*d)*n//2
 
 def calc(n,k,ps):
     fs=get_factors(k,ps)
@@ -52,15 +41,16 @@ def calc(n,k,ps):
         p,s=1,1
         for i,f in enumerate(fs):
             if b>>i&1:
-                p*=f
-                s*=-1
-        res+=s*get_sum(p,n//p,p)
+                p,s=p*f,s*-1
+        res+=s*get_sum(p,p,n//p)
     return res*k
 
 def solve(n,k):
-    ps=get_primes(50000)
-    ds=get_divisors(k)
-    return sum(d*calc(n//d,k//d,ps) for d in ds)%MOD
+    ps=get_primes()
+    res=0
+    for d in get_divisors(k):
+        res+=calc(n//d,k//d,ps)*d
+    return res%MOD
 
 while 1:
     try:
