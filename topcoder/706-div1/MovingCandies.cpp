07fbc342273b20c5fe8c8d370bcf8809b1cfc416
lyoz <lyoz@users.noreply.github.com>
2017-01-22 23:10:07 +0900

    SRM706 Div1 Easy

---
+++ topcoder/706-div1/MovingCandies.cpp
@@ -0,0 +1,330 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned int;
+using ll=long long;
+using ull=unsigned long long;
+using pii=pair<int,int>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+// BEGIN CUT HERE
+#define dump(...) cout<<"# "<<#__VA_ARGS__<<'='<<(__VA_ARGS__)<<endl
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+template<typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	print_tuple<Cdr...>(os,t);
+	os<<(sizeof...(Cdr)?",":"")<<get<sizeof...(Cdr)>(t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<Args...>(os<<'(',t);
+	return os<<')';
+}
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+// END CUT HERE
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+template<typename T>
+struct functor_traits:public functor_traits<decltype(&T::operator())>{};
+template<typename C,typename Ret,typename... Args>
+struct functor_traits<Ret(C::*)(Args...)const>{
+	template<size_t I>
+	using nth_argument_type=typename tuple_element<I,tuple<Args...>>::type;
+	using first_argument_type=nth_argument_type<0>;
+};
+template<typename Compare,typename T=typename decay<typename functor_traits<Compare>::first_argument_type>::type>
+priority_queue<T,vector<T>,Compare> make_priority_queue(Compare comp){
+	return priority_queue<T,vector<T>,Compare>(comp);
+}
+
+struct MovingCandies{
+	int minMoved(vector <string> grid){
+		int h=grid.size(),w=grid[0].size();
+		int total=0;
+		rep(i,h) rep(j,w) total+=grid[i][j]=='#';
+		auto pq=make_priority_queue([](tuple<int,int,int,int> a,tuple<int,int,int,int> b){
+			return get<3>(a)>get<3>(b);
+		});
+		pq.emplace(0,0,grid[0][0]=='#',grid[0][0]=='.');
+		vector<vvi> cnt(h,vvi(w,vi(h*w+1,INF)));
+		while(pq.size()){
+			int ci,cj,cx,cy; tie(ci,cj,cx,cy)=pq.top(); pq.pop();
+			if(cx+cy>total||cnt[ci][cj][cx]!=INF) continue;
+			cnt[ci][cj][cx]=cy;
+			rep(k,4){
+				int ni=ci+"\xff\x1\0\0"[k],nj=cj+"\0\0\xff\x1"[k];
+				if(ni<0||h<=ni||nj<0||w<=nj) continue;
+				pq.emplace(ni,nj,cx+(grid[ni][nj]=='#'),cy+(grid[ni][nj]=='.'));
+			}
+		}
+		int res=INF;
+		for(auto x:cnt[h-1][w-1]) res=min(res,x);
+		return res==INF?-1:res;
+	}
+};
+
+// BEGIN CUT HERE
+#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <vector>
+namespace moj_harness {
+	using std::string;
+	using std::vector;
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				std::cerr << "Illegal input! Test case " << casenum << " does not exist." << std::endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			std::cerr << "No test cases run." << std::endl;
+		} else if (correct < total) {
+			std::cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << std::endl;
+		} else {
+			std::cerr << "All " << total << " tests passed!" << std::endl;
+		}
+	}
+	
+	int verify_case(int casenum, const int &expected, const int &received, std::clock_t elapsed) { 
+		std::cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			std::sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		std::cerr << verdict;
+		if (!info.empty()) {
+			std::cerr << " (";
+			for (size_t i=0; i<info.size(); ++i) {
+				if (i > 0) std::cerr << ", ";
+				std::cerr << info[i];
+			}
+			std::cerr << ")";
+		}
+		std::cerr << std::endl;
+		
+		if (verdict == "FAILED") {
+			std::cerr << "    Expected: " << expected << std::endl; 
+			std::cerr << "    Received: " << received << std::endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			string t[]                = {
+"#...###",
+"#...#.#",
+"##..#.#",
+".#....#"
+};
+			int expected__            = 3;
+			std::clock_t start__      = std::clock();
+			int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			string t[]                = {
+"#...###",
+"#...#.#",
+"##..###",
+".#....#"
+};
+			int expected__            = 2;
+			std::clock_t start__      = std::clock();
+			int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			string t[]                = {
+".#..",
+"##..",
+"..#.",
+"..#.",
+"..##",
+"..##"
+};
+			int expected__            = 2;
+			std::clock_t start__      = std::clock();
+			int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			string t[]                = {
+".....",
+".###.",
+"####.",
+"....."
+};
+			int expected__            = -1;
+			std::clock_t start__      = std::clock();
+			int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 4: {
+			string t[]                = {
+".#...#.###.#",
+"#.#.##......",
+".#.#......#.",
+"..#.......#.",
+"##.........."
+};
+			int expected__            = 9;
+			std::clock_t start__      = std::clock();
+			int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 5: {
+			string t[]                = {
+"###.#########..#####",
+".#######.###########"
+};
+			int expected__            = 0;
+			std::clock_t start__      = std::clock();
+			int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 6: {
+			string t[]                = {
+"..",
+".."
+};
+			int expected__            = -1;
+			std::clock_t start__      = std::clock();
+			int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+		case 7: {
+		   string t[]                = {
+"#......",
+"#......",
+"#......",
+"#.##..#",
+"#.#...#",
+"#.#...#",
+"###...#",
+};
+		   int expected__            = 3;
+		   std::clock_t start__      = std::clock();
+		   int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+		   return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 8: {
+		   string t[]                = {
+"#....##",
+"#......",
+"#......",
+"#.##..#",
+"#.#...#",
+"#.#...#",
+"###...#",
+};
+		   int expected__            = 2;
+		   std::clock_t start__      = std::clock();
+		   int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+		   return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 9: {
+		   string t[]                = {
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+"#.#.#.#.#.#.#.#.#.#.",
+".#.#.#.#.#.#.#.#.#.#",
+};
+		   int expected__            = 19;
+		   std::clock_t start__      = std::clock();
+		   int received__            = MovingCandies().minMoved(vector <string>(t, t + (sizeof t / sizeof t[0])));
+		   return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		default:
+			return -1;
+		}
+	}
+}
+
+#include <cstdlib>
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(std::atoi(argv[i]));
+	}
+}
+// END CUT HERE
