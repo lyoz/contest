6c005cd38f057e118f1e53291150c411382df5ac
lyoz <lyoz@users.noreply.github.com>
2017-03-18 23:58:58 +0900

    ARC 070 Problem CD

---
+++ atcoder/arc070/D.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) cout<<"# "<<#__VA_ARGS__<<'='<<(__VA_ARGS__)<<endl
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	print_tuple<Cdr...>(os,t);
+	os<<(sizeof...(Cdr)?",":"")<<get<sizeof...(Cdr)>(t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+int solve(int n,int k,vi a)
+{
+	sort(all(a));
+	int lo=0,hi=n;
+	while(lo<hi){
+		int mi=(lo+hi)/2;
+		vi dp(k); dp[0]=1;
+		rep(i,n) if(i!=mi){
+			peri(j,a[i],k)
+				dp[j]|=dp[j-a[i]];
+		}
+		if(count(begin(dp)+max(k-a[mi],0),end(dp),1))
+			hi=mi;
+		else
+			lo=mi+1;
+	}
+	return lo;
+}
+
+int main()
+{
+	for(int n,k;cin>>n>>k&&n|k;){
+		vi a(n);
+		rep(i,n) cin>>a[i];
+		cout<<solve(n,k,a)<<endl;
+	}
+}
