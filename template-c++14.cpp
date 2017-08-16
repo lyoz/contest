82c3154d49e29a62d952be403df8c6cbc11a8624
lyoz <lyoz@users.noreply.github.com>
2017-08-16 17:53:28 +0900

    fix template

--- template-c++14.cpp
+++ template-c++14.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
 #define repi(i,a,b) for(int i=int(a);i<int(b);i++)
 #define peri(i,a,b) for(int i=int(b);i-->int(a);)
 #define rep(i,n) repi(i,0,n)
@@ -20,17 +21,17 @@ using vd=vector<double>;
 using vvd=vector<vd>;
 using vs=vector<string>;
 
-$BEGINCUT$
-#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
 void print_vars(ostream&){}
 template<typename Car,typename... Cdr>
 void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
 	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
 }
+
 template<typename T1,typename T2>
 ostream& operator<<(ostream& os,const pair<T1,T2>& p){
 	return os<<'('<<p.first<<','<<p.second<<')';
 }
+
 template<int I,typename Tuple>
 void print_tuple(ostream&,const Tuple&){}
 template<int I,typename Car,typename... Cdr,typename Tuple>
@@ -43,6 +44,7 @@ ostream& operator<<(ostream& os,const tuple<Args...>& t){
 	print_tuple<0,Args...>(os<<'(',t);
 	return os<<')';
 }
+
 template<typename Ch,typename Tr,typename C>
 basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
 	os<<'[';
@@ -50,18 +52,16 @@ basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
 		os<<(i==begin(c)?"":" ")<<*i;
 	return os<<']';
 }
-$ENDCUT$
 
 constexpr int INF=1e9;
 constexpr int MOD=1e9+7;
 constexpr double EPS=1e-9;
 
-struct $CLASSNAME${
-	$RC$ $METHODNAME$($METHODPARMS$){
-	}
-};
-
-$BEGINCUT$
-$TESTCODE$
-$DEFAULTMAIN$
-$ENDCUT$
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+}
