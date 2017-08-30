4342526fb3ebdd9397e603e73bae59bc03f38beb
lyoz <lyoz@users.noreply.github.com>
2017-08-30 22:26:50 +0900

    Codeforces #430 Div2 Problem CD

---
+++ codeforces/842/C.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+void solve(const vvi& g,const vi& a,int p,int u,vi& gcds,vector<set<int>>& cand)
+{
+	if(u==0){
+		gcds[u]=a[u];
+		cand[u].insert(a[u]);
+	}
+	else{
+		gcds[u]=__gcd(gcds[p],a[u]);
+		cand[u].insert(gcds[p]);
+		if(p==0) cand[u].insert(a[u]);
+		for(int x:cand[p])
+			cand[u].insert(__gcd(a[u],x));
+	}
+	for(int v:g[u]) if(v!=p)
+		solve(g,a,u,v,gcds,cand);
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(int n;cin>>n&&n;){
+		vi a(n);
+		rep(i,n) cin>>a[i];
+		vvi g(n);
+		rep(i,n-1){
+			int u,v; cin>>u>>v; u--,v--;
+			g[u].push_back(v);
+			g[v].push_back(u);
+		}
+		vi gcds(n);
+		vector<set<int>> cand(n);
+		solve(g,a,-1,0,gcds,cand);
+		rep(i,n) cout<<*(--end(cand[i]))<<"\n "[i+1<n];
+	}
+}
