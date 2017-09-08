316cd7358dd62983b0132f0b7e816b50140250b5
lyoz <lyoz@users.noreply.github.com>
2017-09-08 23:28:03 +0900

    AtCoder Typical Contest 001 Problem C

---
+++ atcoder/atc001/C.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+constexpr double PI=acos(-1);
+
+using Complex=complex<double>;
+constexpr Complex I(0,1);
+
+void FFT(vector<Complex>& a,int sign){
+	int n=a.size();
+	for(int i=0;i<n;i++){
+		int j=0;
+		for(int k=0;(1<<k)<n;k++)
+			(j<<=1)|=i>>k&1;
+		if(i<j)
+			swap(a[i],a[j]);
+	}
+	for(int m=2;m<=n;m*=2){
+		double theta=sign*2*PI/m;
+		for(int i=0;i<m/2;i++){
+			Complex wi=exp(I*theta*(double)i);
+			for(int j=0;j<n;j+=m){
+				Complex &x=a[i+j],&y=a[i+m/2+j];
+				tie(x,y)=mt(x+y*wi,x-y*wi);
+			}
+		}
+	}
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(int n;cin>>n&&n;){
+		vi a(n+1),b(n+1);
+		rep(i,n) cin>>a[i+1]>>b[i+1];
+
+		int m=1;
+		for(;m<=2*n;m*=2);
+
+		vector<Complex> f(m),g(m),h(m);
+		rep(i,n+1) f[i]=a[i],g[i]=b[i];
+
+		FFT(f,-1);
+		FFT(g,-1);
+		rep(i,m)
+			h[i]=f[i]*g[i];
+		FFT(h,1);
+		rep(i,m) h[i]/=m;
+
+		repi(i,1,2*n+1)
+			cout<<int(real(h[i])+0.5)<<endl;
+	}
+}
