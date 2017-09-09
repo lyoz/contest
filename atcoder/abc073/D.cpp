51d22740a6eb7785cc024e824bab6cd9e2c0c643
lyoz <lyoz@users.noreply.github.com>
2017-09-09 21:44:17 +0900

    ABC073 Problem ABCD

---
+++ atcoder/abc073/D.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(int n,m,r;cin>>n>>m>>r&&n|m|r;){
+		vi rs(r);
+		rep(i,r) cin>>rs[i],rs[i]--;
+		vvi a(n,vi(n,INF));
+		rep(_,m){
+			int u,v,w; cin>>u>>v>>w; u--,v--;
+			a[u][v]=a[v][u]=w;
+		}
+		rep(k,n) rep(i,n) rep(j,n)
+			a[i][j]=min(a[i][j],a[i][k]+a[k][j]);
+
+		vvi dp(r,vi(1<<r,INF));
+		rep(i,r) dp[i][1<<i]=0;
+		repi(i,1,1<<r){
+			rep(j,r) if(i>>j&1)
+				rep(k,r) if(!(i>>k&1))
+					dp[k][i|1<<k]=min(dp[k][i|1<<k],dp[j][i]+a[rs[j]][rs[k]]);
+		}
+		int res=INF;
+		rep(i,r) res=min(res,dp[i].back());
+		cout<<res<<endl;
+	}
+}
