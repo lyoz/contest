7a168013c86b71008dfaa5718534eeb5cc8bbc25
lyoz <lyoz@users.noreply.github.com>
2017-09-11 22:02:02 +0900

    ABC062 Problem ABCD

---
+++ atcoder/abc062/D.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr ll INF=1e18;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+template<typename C>
+vl calc(int n,vi a,C comp)
+{
+	map<int,int,C> f;
+	ll sum=0;
+	rep(i,n){
+		f[a[i]]++;
+		sum+=a[i];
+	}
+	vl res={sum};
+	rep(i,n){
+		if(comp(begin(f)->first,a[n+i])){
+			sum=sum+a[n+i]-begin(f)->first;
+			if(--begin(f)->second==0)
+				f.erase(begin(f));
+			f[a[n+i]]++;
+		}
+		res.push_back(sum);
+	}
+	return res;
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(int n;cin>>n&&n;){
+		vi a(3*n);
+		rep(i,3*n) cin>>a[i];
+
+		vl pre=calc(n,a,less<int>());
+		reverse(all(a));
+		vl suf=calc(n,a,greater<int>());
+
+		ll res=-INF;
+		rep(i,n+1)
+			res=max(res,pre[i]-suf[n-i]);
+		cout<<res<<endl;
+	}
+}
