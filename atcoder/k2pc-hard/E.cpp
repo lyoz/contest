e5676bbbc9267090e069aa7b8381d62f118e6a8e
lyoz <lyoz@users.noreply.github.com>
2017-10-04 19:16:51 +0900

    K2PC Hard Problem E

---
+++ atcoder/k2pc-hard/E.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+//inline int ModPow(int a,int r,int m)
+//{
+//	int x=1;
+//	for(;r;r>>=1){
+//		if(r&1) x=(ll)x*a%m;
+//		a=(ll)a*a%m;
+//	}
+//	return x;
+//}
+
+vvi memo;
+inline int ModPow(int a,int r,int m)
+{
+	int& x=memo[a][r];
+	if(x!=-1) return x;
+	for(x=1;r;r>>=1){
+		if(r&1) x=(ll)x*a%m;
+		a=(ll)a*a%m;
+	}
+	return x;
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(int n,q,p;cin>>n>>q>>p&&n|q|p;){
+		vi a(n);
+		rep(i,n) cin>>a[i];
+
+		vector<tuple<int,int,int>> qs;
+		rep(i,q){
+			int s,t; cin>>s>>t; s--;
+			qs.emplace_back(i,s,t);
+		}
+
+		{
+			memo.clear();
+			vi f(*max_element(all(a))+1);
+			for(auto x:a) f[x]++;
+			memo.resize(f.size());
+			rep(i,f.size()) memo[i].resize(f[i]+1,-1);
+		}
+
+		int bcount=sqrt(q),bsize=(n+bcount-1)/bcount;
+		sort(all(qs),[bsize](auto a,auto b){
+			return mt(get<1>(a)/bsize,get<2>(a))<mt(get<1>(b)/bsize,get<2>(b));
+		});
+
+		vi f(*max_element(all(a))+1);
+		int pl=0,pr=0;
+		ll sum=0;
+		vi res(q);
+		for(auto query:qs){
+			int id,l,r; tie(id,l,r)=query;
+			repi(i,pr,r) sum+=ModPow(a[i],++f[a[i]],p);
+			repi(i,r,pr) sum+=p-ModPow(a[i],f[a[i]]--,p);
+			repi(i,pl,l) sum+=p-ModPow(a[i],f[a[i]]--,p);
+			repi(i,l,pl) sum+=ModPow(a[i],++f[a[i]],p);
+			res[id]=(sum%=p);
+			pl=l,pr=r;
+		}
+		rep(i,q) cout<<res[i]<<endl;
+	}
+}
