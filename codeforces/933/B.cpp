e8a1daf4a9ac12ced8c2a395f46dc69016affb10
lyoz <lyoz@users.noreply.github.com>
2018-02-15 00:39:50 +0900

    Cf#462 Div1 Problem AB

---
+++ codeforces/933/B.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+vl solve(ll p,ll k)
+{
+	vl q;
+	for(ll t=p,sign=-1;;sign*=-1){
+		if(sign==-1)
+			t=t/k;
+		else
+			t=(t+k-1)/k;
+		q.push_back(sign*t);
+		if(sign==1&&0<=t&&t<k)
+			break;
+	}
+	vl f(q.size()+1);
+	f[0]+=p;
+	rep(i,q.size()){
+		f[i]+=q[i]*k;
+		f[i+1]+=q[i];
+	}
+	while(f.size()&&f.back()==0)
+		f.pop_back();
+	return f;
+}
+
+void test()
+{
+	rep(_,10){
+		ll p=1+rand()%100;
+		ll k=2+rand()%99;
+		dump(p,k);
+		dump(solve(p,k));
+	}
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	//test();
+
+	for(ll p,k;cin>>p>>k&&p|k;){
+		vl f=solve(p,k);
+		cout<<f.size()<<endl;
+		rep(i,f.size())
+			cout<<f[i]<<"\n "[i+1<f.size()];
+	}
+}
