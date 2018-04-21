a8b92223d756f2d12dafe7d13dafc37dac530a65
lyoz <lyoz@users.noreply.github.com>
2018-04-22 05:26:46 +0900

    ARC096 Problem CDE

---
+++ atcoder/arc096/D.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr ll INF=1e18;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+ll calc(ll n,ll c,vl xs,vl vs)
+{
+	vl ss(n+1),ts(n+1);
+	rep(i,n) ss[i+1]=ss[i]+vs[i];
+	rep(i,n) ss[i+1]-=2*xs[i];
+	per(i,n) ts[i]=ts[i+1]+vs[i];
+	per(i,n) ts[i]-=c-xs[i];
+
+	vl ps=ss,qs=ts;
+	rep(i,n) chmax(ps[i+1],ps[i]);
+	per(i,n) chmax(qs[i],qs[i+1]);
+
+	ll res=0;
+	rep(i,n+1)
+		chmax(res,ps[i]+qs[i]);
+	return res;
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(ll n,c;cin>>n>>c&&n|c;){
+		vl xs(n),vs(n);
+		rep(i,n) cin>>xs[i]>>vs[i];
+
+		ll res=calc(n,c,xs,vs);
+		reverse(all(xs));
+		rep(i,n) xs[i]=c-xs[i];
+		reverse(all(vs));
+		chmax(res,calc(n,c,xs,vs));
+
+		cout<<res<<endl;
+	}
+}
