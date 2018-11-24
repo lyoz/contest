934ea9afe2468651d7ceee7685c199cf2a743ae7
lyoz <lyoz@users.noreply.github.com>
2018-11-24 22:54:34 +0900

    dwacon 2018 qual problem ABC,D(subtask)

---
+++ atcoder/dwacon5th-prelims/D.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+void solve_subtask(int n,int d,vi xs,vi ys)
+{
+	if(d>30) return;
+
+	vvi ms(d,vi(d));
+	rep(i,n)
+		ms[xs[i]%d][ys[i]%d]++;
+
+	vi sqrs(400);
+	rep(i,sqrs.size()) sqrs[i]=i*i;
+
+	int res=INF;
+	rep(i0,d) rep(j0,d){
+		int mx=0;
+		rep(i,d) rep(j,d){
+			int t=ms[(i0+i)%d][(j0+j)%d];
+			int k=lower_bound(all(sqrs),t)-begin(sqrs);
+			if(t<=k*(k-1))
+				mx=max(mx,(k-1)*d+min(i,j));
+			else
+				mx=max(mx,(k-1)*d+max(i,j));
+		}
+		res=min(res,mx);
+	}
+	cout<<res<<endl;
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(int n,d;cin>>n>>d&&n|d;){
+		vi xs(n),ys(n);
+		rep(i,n) cin>>xs[i]>>ys[i];
+		solve_subtask(n,d,xs,ys);
+	}
+}
