205134944e9c1b587414841ee2d0027ddaced376
lyoz <lyoz@users.noreply.github.com>
2020-04-20 04:32:41 +0900

    CodeJam2020 Round1B Problem AC

---
+++ codejam/2020/round1b/C.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+void solve()
+{
+	int r,s; cin>>r>>s;
+
+	int n=r*s;
+	int m=r*(s-1)/2;
+
+	vi a;
+	rep(i,s) rep(j,r)
+		a.push_back(j);
+
+	vector<tuple<int,int>> res;
+	rep(_,m){
+		int i,j,k;
+		for(i=0;i<n&&a[i]==a[0];i++)
+			;
+		for(j=i;j<n&&a[j]==a[i];j++)
+			;
+		int x=a[0],y=a[i];
+		for(k=j;k<n;k++)
+			if(a[k-1]==x&&a[k]==y)
+				break;
+		rotate(begin(a),begin(a)+j,begin(a)+k);
+		res.emplace_back(j,k-j);
+	}
+
+	if(r*(s-1)%2==1){
+		for(int i=0;i<n;i++)
+			if(a[i]!=a[0]){
+				res.emplace_back(i,n-i);
+				break;
+			}
+	}
+
+	cout<<res.size()<<endl;
+	for(auto p:res)
+		cout<<get<0>(p)<<' '<<get<1>(p)<<endl;
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	int tc; cin>>tc;
+	rep(i,tc){
+		cout<<"Case #"<<i+1<<": ";
+		solve();
+	}
+}
