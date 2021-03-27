58f934ecce2d48a0be15635fcbab7bc08af4c7b7
lyoz <lyoz@users.noreply.github.com>
2021-03-27 16:20:05 +0900

    CodeJam2021 Qual Problem ABC

---
+++ codejam/2021/qual/C.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+int calc(vi a)
+{
+	int n=a.size();
+	int res=0;
+	rep(i,n-1){
+		int j=min_element(i+all(a))-begin(a);
+		reverse(begin(a)+i,begin(a)+j+1);
+		res+=j+1-i;
+	}
+	return res;
+}
+
+void solve()
+{
+	int n,c; cin>>n>>c;
+	if(!(n-1<=c&&c<=n*(n+1)/2-1)){
+		cout<<"IMPOSSIBLE"<<endl;
+		return;
+	}
+	c-=n-1;
+	vi a(n); iota(all(a),1);
+	per(i,n-1){
+		int x=min(n-i-1,c);
+		reverse(begin(a)+i,begin(a)+i+x+1);
+		c-=x;
+	}
+	rep(i,n) cout<<a[i]<<"\n "[i<n-1];
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	int tc; cin>>tc;
+	rep(i,tc){
+		cout<<"Case #"<<i+1<<": ";
+		solve();
+	}
+}
