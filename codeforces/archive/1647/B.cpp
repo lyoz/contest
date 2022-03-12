494e7359da884d73eaaeb058e89808c099ff4fe3
lyoz <lyoz@users.noreply.github.com>
2022-03-13 08:13:24 +0900

    Cf#777 Div2 Problem ABC

---
+++ codeforces/archive/1647/B.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+#define mp make_pair
+#define mt make_tuple
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vs=vector<string>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+bool solve(vs grid)
+{
+	int h=grid.size(),w=grid[0].size();
+	rep(i,h)
+		grid[i]='0'+grid[i]+'0';
+	rep(i,h){
+		repi(j,1,w+1){
+			if(grid[i][j]=='1'){
+				int l=j,r=j;
+				for(;r<w+1&&grid[i][r]=='1';r++)
+					;
+				repi(k,i,h){
+					int cnt=count(begin(grid[k])+l,begin(grid[k])+r,'1');
+					if(cnt==0)
+						break;
+					if(cnt<r-l||grid[k][l-1]=='1'||grid[k][r]=='1')
+						return false;
+					fill(begin(grid[k])+l,begin(grid[k])+r,'0');
+				}
+			}
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+	
+	int tc; cin>>tc;
+	rep(_,tc){
+		int h,w; cin>>h>>w;
+		vs grid(h);
+		rep(i,h) cin>>grid[i];
+		cout<<(solve(grid)?"YES":"NO")<<endl;
+	}
+}
