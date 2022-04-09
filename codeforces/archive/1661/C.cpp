305bea7bb8749174098bcc1ebd421bf037e7dc58
lyoz <1482802+lyoz@users.noreply.github.com>
2022-04-10 01:33:02 +0900

    Educational Cf Round 126 Problem ABCD

---
+++ codeforces/archive/1661/C.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vvvi=vector<vvi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vvvl=vector<vvl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vvvd=vector<vvd>;
+using vs=vector<string>;
+using vvs=vector<vs>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr ll INF=1e18;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+bool ok(const vl& hs,ll target,ll x)
+{
+	ll one=(x+1)/2,two=x/2;
+	rep(i,hs.size()){
+		ll d=target-hs[i];
+		ll usetwo=min(d/2,two);
+		two-=usetwo;
+		d-=usetwo*2;
+		if(d>one)
+			return false;
+		one-=d;
+	}
+	return true;
+}
+
+void solve()
+{
+	int n; cin>>n;
+	vl hs(n);
+	rep(i,n) cin>>hs[i];
+	ll mx=*max_element(all(hs));
+	ll res=INF;
+	for(ll target:vl{mx,mx+1}){
+		ll lo=0;
+		ll hi=[&]{
+			ll cnt=0;
+			rep(i,n)
+				cnt+=target-hs[i];
+			return cnt*2-1;
+		}();
+		while(lo<hi){
+			ll mi=(lo+hi)/2;
+			if(ok(hs,target,mi))
+				hi=mi;
+			else
+				lo=mi+1;
+		}
+		chmin(res,lo);
+	}
+	cout<<res<<endl;
+}
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	int tc; cin>>tc;
+	rep(_,tc)
+		solve();
+}
