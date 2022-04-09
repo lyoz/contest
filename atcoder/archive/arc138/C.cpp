c2899d1f3409fb5c4acf558d7af8ce077d41a878
lyoz <1482802+lyoz@users.noreply.github.com>
2022-04-09 23:26:53 +0900

    ARC138 Problem ABC

---
+++ atcoder/archive/arc138/C.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dump(...) do{print_vars(cout<<"# "<<#__VA_ARGS__<<'=',__VA_ARGS__);cout<<endl;}while(0)
+#define repi(i,a,b) for(int i=int(a);i<int(b);i++)
+#define peri(i,a,b) for(int i=int(b);i-->int(a);)
+#define rep(i,n) repi(i,0,n)
+#define per(i,n) peri(i,0,n)
+#define all(c) begin(c),end(c)
+
+using uint=unsigned;
+using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vvvi=vector<vvi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using vvvl=vector<vvl>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vvvd=vector<vvd>;
+using vs=vector<string>;
+using vvs=vector<vs>;
+
+template<typename T,typename U>bool chmin(T& a,const U& b){return a>b?(a=b,1):0;}
+template<typename T,typename U>bool chmax(T& a,const U& b){return a<b?(a=b,1):0;}
+
+void print_vars(ostream&){}
+template<typename Car,typename... Cdr>
+void print_vars(ostream& os,const Car& car,const Cdr&... cdr){
+	print_vars(os<<car<<(sizeof...(cdr)?",":""),cdr...);
+}
+
+template<typename T1,typename T2>
+ostream& operator<<(ostream& os,const pair<T1,T2>& p){
+	return os<<'('<<p.first<<','<<p.second<<')';
+}
+
+template<int I,typename Tuple>
+void print_tuple(ostream&,const Tuple&){}
+template<int I,typename Car,typename... Cdr,typename Tuple>
+void print_tuple(ostream& os,const Tuple& t){
+	os<<get<I>(t)<<(sizeof...(Cdr)?",":"");
+	print_tuple<I+1,Cdr...>(os,t);
+}
+template<typename... Args>
+ostream& operator<<(ostream& os,const tuple<Args...>& t){
+	print_tuple<0,Args...>(os<<'(',t);
+	return os<<')';
+}
+
+template<typename Ch,typename Tr,typename C>
+basic_ostream<Ch,Tr>& operator<<(basic_ostream<Ch,Tr>& os,const C& c){
+	os<<'[';
+	for(auto i=begin(c);i!=end(c);++i)
+		os<<(i==begin(c)?"":" ")<<*i;
+	return os<<']';
+}
+
+constexpr int INF=1e9;
+constexpr int MOD=1e9+7;
+constexpr double EPS=1e-9;
+
+int main()
+{
+	#ifndef _GLIBCXX_DEBUG
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	constexpr char endl='\n';
+	#endif
+
+	for(int n;cin>>n&&n;){
+		vi a(n);
+		rep(i,n) cin>>a[i];
+
+		ll largerhalf=[&]{
+			vi b=a;
+			sort(all(b));
+			return accumulate(n/2+all(b),0ll);
+		}();
+
+		vector<tuple<int,int>> b;
+		rep(i,n)
+			b.emplace_back(a[i],i);
+		sort(all(b));
+
+		vi rank(n);
+		rep(i,n)
+			rank[get<1>(b[i])]=i;
+
+		vi c(n);
+		rep(i,n){
+			if(i)
+				c[i]=c[i-1];
+			if(i%2==0)
+				c[i]--;
+			if(rank[i]>=n/2)
+				c[i]++;
+		}
+		int mi=max_element(all(c))-begin(c);
+		cout<<(mi+1)%n<<' '<<largerhalf<<endl;
+	}
+}
